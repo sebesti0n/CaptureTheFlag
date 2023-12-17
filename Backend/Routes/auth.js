f4886ae2d3c7281d60a3e56e2abf9ccfd29c5e34
sebesti0n <prashankumar0710@gmail.com>
2023-12-18 02:42:19 +0530

    Implementing JSON Web Token (JWT) for User Authentication and Authorization.

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv =require('dotenv')
+dotenv.config()
 require('../connections/dbConnect')
 const user = require('../models/user')
 router.get('/', (req, res) => {
@@ -10,19 +13,24 @@ router.post('/register', async (req, res, next) => {
     const newUser = await new user(req.body);
     const alreadyPresent = await user.findOne({ Email: newUser.Email })
     if (newUser.password !== newUser.cnfpassword) {
-        return res.status(200).json({message:'confirm password does not match with password'});
+        return res.status(200).json({success:false, message:'confirm password does not match with password'});
     }
     else if (alreadyPresent) {
-        return res.status(200).json({message:'user already registered'});
+        return res.status(200).json({success:false, message:'user already registered'});
     }
     else {
         bcrypt.hash(newUser.password, 10, (err, hashedpassword) => {
             if (err) return next(err);
             newUser.set('password', hashedpassword);
             newUser.set('cnfpassword', hashedpassword);
+            let data={
+                time:Date(),
+                userEmail:newUser.Email
+            }
+            const token = jwt.sign(data, process.env.JWT_SECRET_KEY);
             newUser.save().then(() => {
                 console.log("all is well");
-                return res.status(200).json(newUser);
+                return res.status(200).json({token:token, user: newUser});
             }).catch((e) => {
                 console.log("dikkat");
                 console.log(e);
@@ -33,27 +41,50 @@ router.post('/register', async (req, res, next) => {
 });
 
 
-router.get('/login/:useremail/:pwd', async (req, res) => {
+router.post('/login', async (req, res, next) => {
+    const {userEmail, userPass} = req.body;
     try {
-        const userEmail = req.params.useremail;
-        const userPass = req.params.pwd;
         const finduser = await user.findOne({ Email: userEmail });
         if (finduser) {
             const validate = await bcrypt.compare(userPass, finduser.password);
             if (!validate) {
-                res.status(403).json({ message: 'wrong password' });
+                res.status(403).json({ success : false, message: 'Invalid credential' });
             }
-            else res.status(200).json(finduser);
+            else res.status(200).json({success: true,userDetails:finduser});
         }
         else {
-            res.status(200).json({message:'No User Found'});
+            res.status(200).json({success:false, message:'No User Found'});
         }
     } catch (error) {
         console.log(error);
-        res.status(500).send({message:'Internal Server Error'});
+        res.status(500).send({success:false, message:'Internal Server Error'});
 
     }
 
 });
+
+
+router.get("/validate", (req, res) => {
+    try {
+      let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+      let jwtSecretKey = process.env.JWT_SECRET_KEY;
+      console.log("Headers:", req.headers);
+      const token = req.header('sebestian');
+      if (!token) {
+        return res.status(401).send("Token not provided");
+      }
+  
+      const verified = jwt.verify(token, jwtSecretKey);
+      if (verified) {
+        return res.send("Successfully Verified");
+      } else {
+        console.log("Token not verified");
+        return res.status(401).send("Token not verified");
+      }
+    } catch (error) {
+      console.error("Error validating token:", error.message);
+      return res.status(401).send("Token validation error");
+    }
+  });
 
 module.exports = router;
