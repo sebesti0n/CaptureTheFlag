4ebaf7b50c510326ae775d4f83b9903f76031447
sebesti0n <prashankumar0710@gmail.com>
2023-12-25 19:41:14 +0530

    Remove unneccessary files

--- Frontend/app/src/main/java/com/example/capturetheflag/activities/MainActivity.kt
+++ Frontend/app/src/main/java/com/example/capturetheflag/activities/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         sharedPref =userPreferences(this)
