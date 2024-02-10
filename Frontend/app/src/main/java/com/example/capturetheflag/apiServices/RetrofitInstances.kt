97bfc50b231fde09efa0da6ad952df57845f385b
Tejas-67 <tejasjha3210@gmail.com>
2024-02-10 22:43:12 +0530

    fix: read, write timeout in retrofir

--- Frontend/app/src/main/java/com/example/capturetheflag/apiServices/RetrofitInstances.kt
+++ Frontend/app/src/main/java/com/example/capturetheflag/apiServices/RetrofitInstances.kt
@@ -3,16 +3,20 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 class RetrofitInstances {
     companion object {
-        private val BASEURL="https://bd2e-49-38-243-144.ngrok-free.app/"
+        private val BASEURL="https://ctf-ndp3.onrender.com"
         private val retrofit: Retrofit by lazy {
             val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(interceptor)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .connectTimeout(120, TimeUnit.SECONDS)
                 .build()
             Retrofit.Builder().baseUrl(BASEURL)
                 .addConverterFactory(GsonConverterFactory.create())
