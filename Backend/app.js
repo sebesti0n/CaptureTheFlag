5e7274d834151fa37be29e9750af01f235cb0228
Prashan Kumar <114914556+sebesti0n@users.noreply.github.com>
2024-02-13 11:01:53 +0530

    implemented cors (#21)
    * Alter Event Table and enhance endpoint
    
    * Add cors init

--- Backend/app.js
+++ Backend/app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const routes=require('./Routes/auth')
 const eventRouter = require('./Routes/event')
 const adminRouter = require('./Routes/admin');
+const cors = require('cors');
 const validationMiddleware = require('./middlewares/validationMiddleware');
 const knex = require('knex')(require('./Configuration/knexfile')['development']);
 const app= express();
 const PORT = 3008;
-
+app.use(cors());
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
