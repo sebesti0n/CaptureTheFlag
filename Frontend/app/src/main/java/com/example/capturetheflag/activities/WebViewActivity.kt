d866a4a3bb452757c4cb65b1477ad41e1a703bf7
sebesti0n <prashankumar0710@gmail.com>
2024-02-13 19:39:15 +0530

    Add web View Activity

---
+++ Frontend/app/src/main/java/com/example/capturetheflag/activities/WebViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capturetheflag.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capturetheflag.R
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}
